9bb9f0bb637b26498f9609c71d753e6fff568672
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-11-02 11:21:38 +1000

    first commit

---
+++ app/Http/Controllers/APIHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APIHandler extends Controller
+{
+    public function getProfile(){
+        dd('test'); 
+    }
+}
