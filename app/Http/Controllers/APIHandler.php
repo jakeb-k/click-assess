d49bcfbe5b9a94ef4126b53ddd571eaabcb43f4e
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-11-02 11:34:48 +1000

    API Get setup

--- app/Http/Controllers/APIHandler.php
+++ app/Http/Controllers/APIHandler.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class APIHandler extends Controller
 {
     public function getProfile(){
-        dd('test'); 
+        $symbol = 'AMD'; 
+       
+        $res = Http::get('https://financialmodelingprep.com/api/v3/profile/'.$symbol.'?apikey=rjtxIVjGarzsucbadQiUQSZ2igNCPoZq');
+        dd($res->body());
+    }
+    public function getQuote(){
+        $symbol = 'AMD';
+
+        $res = Http::get('https://financialmodelingprep.com/api/v3/quote/'.$symbol.'?apikey=rjtxIVjGarzsucbadQiUQSZ2igNCPoZq');
+        dd($res->body());
+
     }
 }
